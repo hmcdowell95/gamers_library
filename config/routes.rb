e3d351182eb688583b9b50e1e93dcf809daf7ff9
hmcdowell95 <hmcdowell95@github.com>
2020-07-03 16:01:09 -0400

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     resources :systems, only: [:index, :show]
     resources :games, only: [:index, :show]
   end
-  resources :systems
+  resources :systems do
+    resources :games, only: [:new, :edit]
+  end
   resources :games
 end
