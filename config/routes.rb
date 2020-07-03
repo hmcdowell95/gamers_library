ee1fcdb7e8cadd9e86e293f8843a95c94ad9ec56
hmcdowell95 <hmcdowell95@github.com>
2020-07-03 00:20:55 -0400

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :systems, only: [:index, :show]
+    resources :games, only: [:index, :show]
+  end
+  resources :systems
+  resources :games
 end
