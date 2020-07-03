7d002f9686f595ecfeb410bb694d8eb7822ceff6
hmcdowell95 <hmcdowell95@github.com>
2020-07-03 00:58:46 -0400

    create validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,6 @@ class Game < ApplicationRecord
     belongs_to :system
     has_many :user_games
     has_many :users, through: :user_games
+    validates :name, presence: true
+    validates :rating, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
     has_many :user_games
     has_many :systems, through: :user_systems
     has_many :games, through: :user_games
+    has_secure_password
+    validates :username, presence: true
+    validates :username, uniqueness: true
 end
