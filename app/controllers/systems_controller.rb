71a59c9bb14f52d7c14b38476f781f1d1ee91cac
hmcdowell95 <hmcdowell95@github.com>
2020-07-04 15:19:22 -0400

    add to seed file

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -1,4 +1,5 @@
 class SystemsController < ApplicationController
+    before_action :logged_in?
 
     def index
         @systems = System.all
@@ -6,6 +7,12 @@ class SystemsController < ApplicationController
 
     def show
         @system = System.find(params[:id])
+    end
+
+    private
+    
+    def logged_in?
+        return head(:forbidden) unless session.include? :user_id
     end
 
 end
--- app/models/system.rb
+++ app/models/system.rb
@@ -2,7 +2,7 @@ class System < ApplicationRecord
     has_many :games
     has_many :user_systems
     has_many :users, through: :user_systems
-    validates :name, presence: true
-    validates :name, uniqueness: true
-    validates :company, presence: true 
+    # validates :name, presence: true
+    # validates :name, uniqueness: true
+    # validates :company, presence: true 
 end
