b2bcf4a7028b5e39d830169776e689646a25a357
hmcdowell95 <hmcdowell95@github.com>
2020-07-04 13:19:13 -0400

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   resources :users do
     resources :systems, only: [:index, :show]
-    resources :games, only: [:index, :show]
+    resources :games, only: [:index, :show, :new, :edit]
   end
-  resources :systems do
-    resources :games, only: [:new, :edit]
-  end
+  resources :systems
   resources :games
 end
