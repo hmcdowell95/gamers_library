07b629a89b7d52fb2a1cf82efdb0e522966d437a
hmcdowell95 <hmcdowell95@github.com>
2020-07-07 00:03:38 -0400

    create game controller new and create methods

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
     before_action :logged_in
 
     def index
-        if params[:user_id]
+        if params[:user_id].to_i == session[:user_id]
             @user = User.find_by(id: params[:user_id])
             if @user.nil?
               redirect_to users_path, alert: "User not found"
@@ -15,9 +15,20 @@ class GamesController < ApplicationController
     end
 
     def new 
+        if params[:user_id].to_i == session[:user_id] && !User.exists?(params[:user_id])
+            redirect_to users_path, alert: "User not found."
+        else
+            @game = Game.new(user_id: params[:user_id])
+        end
     end
 
     def create
+        @game = Game.new(game_params)
+        if @game.save
+            redirect_to game_path(@game)
+        else
+            render :new
+        end
     end
 
     def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :users do
-    resources :systems, only: [:index, :show]
     resources :games, only: [:index, :show, :new, :edit]
   end
   resources :systems, only: [:index, :show]
