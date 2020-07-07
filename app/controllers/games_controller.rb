c4feee7da2a1541e19019a42aebe20cdb2c6b3c1
hmcdowell95 <hmcdowell95@github.com>
2020-07-06 23:06:24 -0400

    create game controller index

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,17 @@
 class GamesController < ApplicationController
+
+
     def index
+        if params[:user_id]
+            @user = User.find_by(id: params[:user_id])
+            if @user.nil?
+              redirect_to users_path, alert: "User not found"
+            else
+              @games = @user.games
+            end
+        else
+            @games = Game.all
+        end
     end
 
     def new 
