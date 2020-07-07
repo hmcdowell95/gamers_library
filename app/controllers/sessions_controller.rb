2825ea941acdb7baa062ea5c4e5658e3c1c74444
hmcdowell95 <hmcdowell95@github.com>
2020-07-07 01:06:23 -0400

    create session methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,15 @@ class SessionsController < ApplicationController
     end
 
     def create
+        user = User.find_by(name: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+          session[:user_id] = user.id
+        else
+            redirect_to "/login"
+        end
+    end
+
+    def destroy
+        session.delete :user_id
     end
 end
